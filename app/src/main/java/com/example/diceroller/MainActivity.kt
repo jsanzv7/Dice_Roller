c9192939186f2d6b66069ca0964dcc0201a65b4b
Elizabeth Sanz <esanz91@gmail.com>
2022-03-28 11:13:33 -0400

    create helper function

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 /**
  * This activity allows the user to roll a dice and view the result
@@ -12,9 +11,14 @@ import android.widget.TextView
  */
 class MainActivity : AppCompatActivity() {
 
+    // declare and initialize views top class level
     private val rollButton: Button by lazy { findViewById(R.id.button) }
     private val diceImage: ImageView by lazy { findViewById(R.id.imageView) }
     private val diceImage2: ImageView by lazy { findViewById(R.id.imageView2) }
+
+    // declare and initialize model top class level to reuse entire activity
+    private val die1 = Dice(6)
+    private val die2 = Dice(6)
 
     /**
      * This method is called when the Activity is created.
@@ -33,37 +37,22 @@ class MainActivity : AppCompatActivity() {
      * Roll the dice and update the screen with the result.
      */
     private fun rollDice() {
-        // Create new Dice object with 6 sides and roll it
-        val dice = Dice(6)
-        val dice2 = Dice(6)
-        val diceRoll = dice.roll()
-        val diceRoll2 = dice2.roll()
+        updateDie(diceImage, die1.roll())
+        updateDie(diceImage2, die2.roll())
+    }
 
-        // Determine which drawable resource ID to use based on the dice roll
-        val drawableResource = when (diceRoll){
+    // reusable function to apply to each die
+    private fun updateDie(die: ImageView, dieRoll: Int) {
+        val drawable = when(dieRoll) {
             1 ->R.drawable.dice_1
             2 ->R.drawable.dice_2
             3 ->R.drawable.dice_3
             4 ->R.drawable.dice_4
             5 ->R.drawable.dice_5
-            else -> R.drawable.dice_6
+            else ->R.drawable.dice_6
         }
-        val drawableResource2 = when (diceRoll2){
-            1 ->R.drawable.dice_1
-            2 ->R.drawable.dice_2
-            3 ->R.drawable.dice_3
-            4 ->R.drawable.dice_4
-            5 ->R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
-
-        // Update the ImageView with the correct drawable resource ID
-        diceImage.setImageResource(drawableResource)
-        diceImage2.setImageResource(drawableResource2)
-
-        // Update the content description
-        diceImage.contentDescription = diceRoll.toString()
-        diceImage2.contentDescription = diceRoll2.toString()
+        die.setImageResource(drawable)
+        die.contentDescription = dieRoll.toString()
     }
 }
 /**
